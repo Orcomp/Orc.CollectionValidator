0a1931ae7782020a1b7b1cf15409929f2c7e1549
tchief <tchief@users.noreply.github.com>
2013-12-13 14:37:19 +0200

    Create build.fsx

---
+++ build.fsx
@@ -0,0 +1,43 @@
+#r @"tools\FAKE\tools\FakeLib.dll"
+
+open System
+open Fake
+
+// --------------------------------------------------------------------------------------
+// Definitions
+
+
+// --------------------------------------------------------------------------------------
+// Clean build results
+
+
+// --------------------------------------------------------------------------------------
+// Build projects
+
+
+// --------------------------------------------------------------------------------------
+// Run tests
+
+
+// --------------------------------------------------------------------------------------
+// Build a NuGet package
+
+
+// --------------------------------------------------------------------------------------
+// Combined targets
+
+Target "Clean" DoNothing
+
+Target "Build" DoNothing
+
+Target "Tests" DoNothing
+
+Target "All" DoNothing
+"Clean" ==> "All"
+"Build" ==> "All"
+"Tests" ==> "All"
+
+Target "Release" DoNothing
+"All" ==> "Release"
+ 
+RunTargetOrDefault "All"
