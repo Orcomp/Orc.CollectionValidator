b2bcb34433f67c7f43e3413b98d24c85ee143133
tchief <cyber.tchief@gmail.com>
2013-12-14 01:46:02 +0200

    Updated paths.

--- publish.bat
+++ publish.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 if not exist tools\FAKE\tools\Fake.exe ( 
-  .nuget\nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion -Prerelease
+  src\.nuget\nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion -Prerelease
 )
 tools\FAKE\tools\Fake.exe build.fsx %* Release
 rem tools\FAKE\tools\Fake.exe build.fsx %* Release "nugetkey=invalidaccesskey"
