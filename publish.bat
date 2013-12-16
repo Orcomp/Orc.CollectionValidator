bcd806bc78dfde6ef9111c532f8cdfb50783d039
tchief <cyber.tchief@gmail.com>
2013-12-16 23:25:00 +0200

    Added log build output to build-log.xml.

--- publish.bat
+++ publish.bat
@@ -3,6 +3,6 @@ cls
 if not exist tools\FAKE\tools\Fake.exe ( 
   src\.nuget\nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion -Prerelease
 )
-tools\FAKE\tools\Fake.exe build.fsx %* Release
-rem tools\FAKE\tools\Fake.exe build.fsx %* Release "nugetkey=invalidaccesskey"
+tools\FAKE\tools\Fake.exe build.fsx %* Release "logfile=build-log.xml"
+rem tools\FAKE\tools\Fake.exe build.fsx %* Release "nugetkey=invalidaccesskey" "logfile=build-log.xml"
 pause
